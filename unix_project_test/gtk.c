99a4e2d2fa923c3f868815bb8febb66d73a372dc
minyou2675 <kymyy2675@gmail.com>
2022-12-04 01:08:10 +0900

    list append error...helpgit add .git add .

--- unix_project_test/gtk.c
+++ unix_project_test/gtk.c
@@ -2,6 +2,13 @@
 #include <sqlite3.h>
 #include <stdio.h>
 //gcc -o gtk gtk.c `pkg-config --libs --clfags gtk+-2.0`
+int callback(int, char **, char **);
+enum{
+ LIST_ID,
+ LIST_NAME,
+ LIST_PRICE,
+ N_COLUMNS
+};
 
 static int counter = 0;
 
@@ -13,26 +20,58 @@ void greet(GtkWidget* widget, gpointer data)
 	g_print("Welcome to GTK\n");
 	g_print("%s clicked %d times\n",(char*)data, ++counter);
 }
-void lookup(GtkWidget* widget,GtkWidget* window){
-	gtk_window_close(window);
-	
+void lookup(){
 	GtkListStore* store;
-	GtkWidget* list, *window2;
-	GtkCellRender *renderer;
+	GtkWidget* list, *window2,*vbox,*label;
+	GtkCellRenderer *renderer;
 	GtkTreeViewColumn *column;
 	GtkTreeSelection* selection;
 	sqlite3* db;
+	char* err_msg = 0;
 	
+	//window 
 	window2 = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_title(GTK_WINDOW(window2),"view");
-	gkt_window_set_default_size(GTK_WINDOW(window2), 512,512);
+	gtk_window_set_default_size(GTK_WINDOW(window2), 512,512);
 	gtk_window_set_position(GTK_WINDOW(window2), GTK_WIN_POS_CENTER);
-	g_signal_connect(window2, "destroy", G_CALLBACK(gtk_main_quit),null);
-	rc = sqlite3_open("test.db",&db);
+	g_signal_connect(window2, "destroy", G_CALLBACK(gtk_main_quit),NULL);
+	//db
+	int rc = sqlite3_open("test.db",&db);
 	char* query = "SELECT * FROM MENU;";
-	sqlite3.exec(rc,query,0,0);
-	if(rc != 
+	rc = sqlite3_exec(db,query,callback,store,&err_msg);
+	if(rc != SQLITE_OK){
+		perror("db");
+		sqlite3_free(err_msg);
+		sqlite3_close(db);
+		exit(1);
+	}
 
+	//create list_view
+	list = gtk_tree_view_new();
+	//columns with rendering
+	store = gtk_list_store_new(N_COLUMNS, G_TYPE_STRING, G_TYPE_STRING, G_TYPE_STRING);
+	renderer = gtk_cell_renderer_text_new();
+	column = gtk_tree_view_column_new_with_attributes("ID", renderer,"text",LIST_ID,
+			NULL);
+	gtk_tree_view_append_column(GTK_TREE_VIEW(list), column);
+	renderer = gtk_cell_renderer_text_new();
+	column = gtk_tree_view_column_new_with_attributes("NAME",renderer,"text",LIST_NAME,NULL);
+	gtk_tree_view_append_column(GTK_TREE_VIEW(list), column);
+	renderer = gtk_cell_renderer_text_new();
+	column = gtk_tree_view_column_new_with_attributes("PRICE",renderer,"text",LIST_PRICE,NULL);
+	g_object_unref(store);
+	//setup the ui
+	gtk_tree_view_set_headers_visible(GTK_TREE_VIEW(list), FALSE);
+	vbox = gtk_vbox_new(FALSE,0);
+	gtk_box_pack_start(GTK_BOX(vbox), list, TRUE, TRUE, 5);
+	label = gtk_label_new("");
+	gtk_box_pack_start(GTK_BOX(vbox), label, FALSE, FALSE, 5);
+	gtk_container_add(GTK_CONTAINER(window2), vbox);
+	
+	gtk_widget_show_all(window2);
+	gtk_main();
+	
+	
 }
 void insert_data(){
 }
@@ -92,7 +131,7 @@ int main(int argc, char* argv[]){
 	//SIGNAL_PART
 	g_signal_connect(window,"delete-event",G_CALLBACK(gtk_main_quit),NULL);
 	g_signal_connect(GTK_OBJECT(button1), "clicked", G_CALLBACK(greet),"button");
-	g_signal_connectt(GTK_OBJECT(button4),"clicked", G_CALLBACK(lookup),"button");
+	g_signal_connect(GTK_OBJECT(button4),"clicked", G_CALLBACK(lookup),"button");
 	//CONTAINER PART
 	gtk_container_add(GTK_CONTAINER(window), frame);
 	gtk_fixed_put(GTK_FIXED(frame),button1,50, 20);
@@ -104,4 +143,20 @@ int main(int argc, char* argv[]){
         return 0;
 
 
+}
+
+int callback(int argc, char **argv, char **azColName){
+	GtkTreeIter iter;
+	GtkListStore * list_store;
+	list_store = gtk_list_store_new(LIST_ID,LIST_NAME,LIST_PRICE,N_COLUMNS);
+	for(int i = 0; i < argc; i++){
+		printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+	}
+	printf("\n");
+	gtk_list_store_append(list_store, &iter);
+	gtk_list_store_set(list_store, &iter, LIST_ID, argv[0],
+			LIST_NAME, argv[1],
+			LIST_PRICE, argv[2],
+			-1);
+	return 0;
 }
