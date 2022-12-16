f2cd9b03e19c60b522167e93936fa35c8fd9dfdc
201711008 <kymyy2675@gmail.com>
2022-12-16 18:34:17 +0900

    thank you ai

--- yumin/main_server.c
+++ yumin/main_server.c
@@ -8,63 +8,65 @@
 #include  "db2.h"
 
 int main(){
-        char buf[1024];
-        
-        int fd1[2];
-	char* sql = "select name,price from menu;";
-        char* err_msg = 0;
-	    sqlite3* db;
-        
-	    // scanf("%s",menu_name);
-	    int rc = sqlite3_open("menu.db", &db);
-	 
-
-	    if (rc != SQLITE_OK)
-	    {
-		perror("DB");
-		sqlite3_close(db);
-		exit(1);
-		}
-       // if(mkfifo("menu_pipe",0666) == -1){
-           // perror("mkfifo");
-         //   exit(1);
-       // }
-        rc = sqlite3_exec(db,sql,Read,0,&err_msg);
-	puts("sql");
-
-	if(pipe(fd1) == -1){
-		perror("pipe");
-		exit(1);
-	}
-	int fd2 = dup(fd1[0]);
-	int fd3 = dup(fd1[1]);
-
-        if((fd2  = open("menu.txt",O_RDONLY)) == -1){
-            perror("open1");
-            exit(1);
-        }
-	puts("menu.txt open");
-	puts("read fd1");
-        if((read(fd2,buf,sizeof(buf))) < 0){
-            perror("read");
-            exit(1);
-        }
-	close(fd2);
-	puts("read fd2");
+       int fd1[2];
+    char* sql = "select name,price from menu;";
+    char* err_msg = 0;
+    sqlite3* db;
     
-        if((fd3 = open("menu_pipe",O_WRONLY)) == -1){
-            perror("open2");
-            exit(1);
+    // scanf("%s",menu_name);
+    int rc = sqlite3_open("menu.db", &db);
+
+
+    if (rc != SQLITE_OK)
+    {
+        perror("DB");
+        sqlite3_close(db);
+        exit(1);
         }
-	puts("open menu_pipe");
-        if((write(fd3,buf,strlen(buf))) < 0 ){
-            perror("read");
-            exit(1);
-        } 
+   // if(mkfifo("menu_pipe",0666) == -1){
+       // perror("mkfifo");
+     //   exit(1);
+   // }
+    rc = sqlite3_exec(db,sql,Read,0,&err_msg);
+puts("sql");
 
-	printf("exit\n");
-        
-	close(fd3);
-	return 0;
+if(pipe(fd1) == -1){
+        perror("pipe");
+        exit(1);
+    }
+int fd2 = dup(fd1[0]);
+int fd3 = dup(fd1[1]);
 
+if((fd2  = open("menu.txt",O_RDONLY)) == -1){
+        perror("open1");
+        exit(1);
+    }
+puts("menu.txt open");
+puts("read fd1");
+if((read(fd2,buf,sizeof(buf))) < 0){
+        perror("read");
+        exit(1);
+    }
+close(fd2);
+puts("read fd2");
+
+if(mkfifo("menu_pipe",0666) == -1){
+        perror("mkfifo");
+        exit(1);
+    }
+
+if((fd3 = open("menu_pipe",O_RDWR)) == -1){
+        perror("open2");
+        exit(1);
+    }
+puts("open menu_pipe");
+if((write(fd3,buf,strlen(buf))) < 0 ){
+        perror("read");
+        exit(1);
+    } 
+
+printf("exit\n");
+    
+close(fd3);
+return 0;
 }
