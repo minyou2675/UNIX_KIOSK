0076461c3c3ac16ea04f8243896b29aabba68eee
201711008 <kymyy2675@gmail.com>
2022-12-16 14:25:31 +0900

    updt

--- yumin/db.c
+++ yumin/db.c
@@ -12,35 +12,48 @@ int Read(void*, int, char**, char** );
 
 //gcc -o db db.c -lsqlite3 -std=c99
 int main(void){
-	char* err_msg = 0;
-	sqlite3* db;
-	int rc = sqlite3_open("test.db", &db);
-	sqlite3_stmt* res;
+	int main(void)
+{
+    sqlite3 *db;
+    char *err_msg = 0;
+    
+    int rc = sqlite3_open("test.db", &db);
+    
+    if (rc != SQLITE_OK)
+    {
+        fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        
+        return 1;
+    }
+    
+    char *sql = "DROP TABLE IF EXISTS MENU;" 
+                "CREATE TABLE MENU(Id INT, Name char(20), Price INT);" 
+                "INSERT INTO MENU VALUES(1, 'pizza', 52642);" 
+                "INSERT INTO MENU VALUES(2, 'pasta', 57127);" 
+                "INSERT INTO MENU VALUES(3, 'hamburger', 9000);" 
+                "INSERT INTO MENU VALUES(4, 'cake', 29000);" 
+                "INSERT INTO MENU VALUES(5, 'salad', 350000);" 
+                "INSERT INTO MENU VALUES(6, 'chicken', 21000);" 
+                "INSERT INTO MENU VALUES(7, 'noodle', 41400);" 
+                "INSERT INTO MENU VALUES(8, 'juice', 21600);";
 
-	if (rc != SQLITE_OK)
-	{
-		perror("DB");
-		sqlite3_close(db);
-		exit(1);
-		}
-	rc = sqlite3_prepare_v2(db,"SELECT SQLITE_VERSION()",-1,&res,0);
-	if (rc != SQLITE_OK)
-	{
-		perror("DB");
-		sqlite3_close(db);
-		exit(1);
-	}
-	char *sql = "DROP TABLE IF EXISTS menu;"
-		"CREATE TABLE menu(id int primary key, name varchar(2),price int,available int));""insert into menu(name,price,available) values ('pizza','25000',1);";
-	rc = sqlite3_exec(db,sql,0,0,&err_msg);
-
-	sql = "select * from menu;";
-
-	rc = sqlite3_exec(db,sql,Read,0,&err_msg);
-
-	sqlite3_close(db);
-	return 0;
-
+    rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
+    
+    if (rc != SQLITE_OK )
+    {
+        fprintf(stderr, "SQL error: %s\n", err_msg);
+        
+        sqlite3_free(err_msg);        
+        sqlite3_close(db);
+        
+        return 1;
+    } 
+    
+    sqlite3_close(db);
+    
+    return 0;
+}
 }
 //search all data
 
@@ -53,7 +66,7 @@ int Read(void* NotUsed, int argc, char** argv, char** azColName)
 	}
 
 	NotUsed = 0;
-	char buf[1024];
+	char buf[1024] = "db \n";
 	for(int i = 0; i < 2; i++){
 		printf("columns %s \n",azColName[i]);
 		strcat(buf,azColName[i]);
